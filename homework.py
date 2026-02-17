44bef58b9606ec1972598930316450abbbc85326
devansh8080gojo-png <devansh.8080.gojo@gmail.com>
2026-02-17 18:17:54 +1030

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,19 @@
+#assignign diffrent variables
+Fav_hobby="soccer"
+Livs_in_AUS=True
+Family_members= 4
+Place_of_birth="New Delhi, India"
+Grade= 7
+
+#printing diffrent variables and data types
+print("Fav hobby", Fav_hobby)
+print("data type of fav_hoddy is", type (Fav_hobby))
+
+print("Lives in AUS", Livs_in_AUS)
+print("data type of lives_in_AUS is", type (Livs_in_AUS))
+
+print("Family members", Family_members)
+print("data type of Family_members is ", type (Family_members))
+
+print("Grade", Grade)
+print("data types of Grade", type (Grade))
